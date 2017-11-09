d8c16d4da01ab776b0442f711324862ad4f8cd20
Maftouhou Hassani <maftouh.hassane@gmail.com>
2017-11-09 17:55:18 +0100

    Creating componants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// Componants
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Componants/header/header.component';
+import { NavComponent } from './Componants/nav/nav.component';
+import { FooterComponent } from './Componants/footer/footer.component';
+import { InscriptionComponent } from './Componants/inscription/inscription.component';
+import { ConnexionComponent } from './Componants/connexion/connexion.component';
+import { MouvementComponent } from './Componants/mouvement/mouvement.component';
+import { ParametreComponent } from './Componants/parametre/parametre.component';
+import { ContactComponent } from './Componants/contact/contact.component';
+import { HomeComponent } from './Componants/home/home.component';
+import { BashboardComponent } from './Componants/bashboard/bashboard.component';
+import { ProfileComponent } from './Componants/profile/profile.component';
+import { DashboardComponent } from './Componants/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'inscription', component: InscriptionComponent},
+    {path: 'connexion', component: ConnexionComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'profle', component: ProfileComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        NavComponent,
+        FooterComponent,
+        InscriptionComponent,
+        ConnexionComponent,
+        MouvementComponent,
+        ParametreComponent,
+        ContactComponent,
+        HomeComponent,
+        BashboardComponent,
+        ProfileComponent,
+        DashboardComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
