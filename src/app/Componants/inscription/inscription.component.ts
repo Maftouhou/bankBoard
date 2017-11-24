d3f93b49db4bd757f25a2f059d4bef004d141d5e
Maftouhou Hassani <maftouh.hassane@gmail.com>
2017-11-24 11:51:01 +0100

    secure user password inputs

--- src/app/Componants/inscription/inscription.component.ts
+++ src/app/Componants/inscription/inscription.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ValidateService} from '../../Services/validate.service';
 import {AuthentificationService} from '../../Services/authentification.service';
-import { FlashMessagesService } from 'angular2-flash-messages';
+import {FlashMessagesService} from 'angular2-flash-messages';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-inscription',
-  templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.css']
+    selector: 'app-inscription',
+    templateUrl: './inscription.component.html',
+    styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
 
@@ -16,16 +16,29 @@ export class InscriptionComponent implements OnInit {
     phone: string;
     email: string;
     password: string;
-    
-    constructor(private validateService: ValidateService,
-                private flashMessages: FlashMessagesService,
-                private authentificationService: AuthentificationService,
-                private router: Router) { }
 
-    ngOnInit() { }
+    passwordMach: boolean;
+    passwordClass: string;
+
+    constructor(private validateService: ValidateService,
+        private flashMessages: FlashMessagesService,
+        private authentificationService: AuthentificationService,
+        private router: Router) {}
+
+    ngOnInit() {}
+
+    userPassWord(ev: any) {
+        if (ev.target.value === this.password) {
+            this.passwordMach = true;
+            this.passwordClass = 'password_mach';
+        } else {
+            this.passwordMach = false;
+            this.passwordClass = 'password_not_mach';
+        }
+    }
 
     onRegisterSubmit() {
-        
+
         const user = {
             firstName: this.firstName,
             lastName: this.lastName,
@@ -33,40 +46,43 @@ export class InscriptionComponent implements OnInit {
             email: this.email,
             password: this.password
         };
-        
+
         // Required Field
-        if (!this.validateService.validateRegister(user)){
+        if (!this.validateService.validateRegister(user)) {
             console.log(user);
-            this.flashMessages.show('fill in all the field',
-            {cssClass: 'alert-info', timeout: 3000}
+            this.flashMessages.show('Merci de remplir tout les champs',
+                {cssClass: 'errorInfo', timeout: 3000}
             );
-        }
-        
+        }else if (!this.validateService.validateEmail(user.email)) {
         // Required correct email
-        if (!this.validateService.validateEmail(user.email)){
             console.log(user);
-            this.flashMessages.show('use a valide email',
-            {cssClass: 'alert-info', timeout: 3000}
+            this.flashMessages.show('Veiller entrer un email valide',
+                {cssClass: 'errorInfo', timeout: 3000}
             );
+        }else if (!this.passwordMach){
+        // validate user password
+            this.flashMessages.show('Votre mot de passe ne correspond pas',
+                {cssClass: 'errorInfo', timeout: 6000}
+            );
+        }else{
+            this.authentificationService.registerUser(user).subscribe(data => {
+                // Register user
+                console.log(data.status);
+
+                if (data.status === undefined) {
+                    this.flashMessages.show('L\'utilisateur ' + data.firstName + ' est créé.',
+                        {cssClass: 'successInfo', timeout: 6000});
+                    this.router.navigate(['/connexion']);
+                } else if (data.status !== undefined) {
+                    this.flashMessages.show(data.status + " : " + data.message,
+                        {cssClass: 'errorInfo', timeout: 6000});
+                    this.router.navigate(['/inscription']);
+                } else {
+                    this.flashMessages.show('Quelque chose a du mal se passé... merci de refaire l\'opperation.',
+                        {cssClass: 'errorInfo', timeout: 6000});
+                    this.router.navigate(['/inscription']);
+                }
+            });
         }
-        
-        // Register user
-        this.authentificationService.registerUser(user).subscribe(data =>{
-            console.log(data.status);
-            
-            if(data.status === undefined){
-                this.flashMessages.show('User ' + data.firstName + ' is created',
-                    {cssClass: 'alert-success', timeout: 6000} );
-                this.router.navigate(['/connexion']);
-            }else if(data.status !== undefined){
-                this.flashMessages.show(data.status + " : " + data.message,
-                    {cssClass: 'alert-danger', timeout: 6000} );
-                this.router.navigate(['/inscription']);
-            }else{
-                this.flashMessages.show("Something went wrong !!!",
-                    {cssClass: 'alert-danger', timeout: 6000} );
-                this.router.navigate(['/inscription']);
-            }
-        });
     }
 }
--- src/app/Componants/nav/nav.component.ts
+++ src/app/Componants/nav/nav.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthentificationService} from '../../Services/authentification.service';
-import { FlashMessagesService } from 'angular2-flash-messages';
+import {FlashMessagesService} from 'angular2-flash-messages';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+    
     constructor(private authentification: AuthentificationService,
-            private router: Router,
-            private flashMessagesService: FlashMessagesService
-            ) { }
+        private router: Router,
+        private flashMessagesService: FlashMessagesService
+    ) {}
 
     ngOnInit() {
     }
-    
-    onLogoutClick(){
+
+    onLogoutClick() {
         this.authentification.logout();
         this.flashMessagesService.show('Logout successful', {
             cssClass: 'alert-success',
             timeout: 6000
         });
-        
+
         this.router.navigate(['connexion']);
         return false;
     }
-    
-    getAllUser(){
+
+    getAllUser() {
         this.authentification.getProfile();
     }
 
